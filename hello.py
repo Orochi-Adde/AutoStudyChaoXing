195777487a87ae7c6abb1ca04e8d60ec034328d5
Orochi-Adde <ad1722932474@gmail.com>
2023-05-12 19:34:02 +0800

    测试

---
+++ hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@Time     : 2023/5/12 19:33
+@Author   : Orochi-Adde
+@File     : hello.py
+@Software : PyCharm
+@Github   : https://github.com/Orochi-Adde
+"""
+if __name__ == '__main__':
+    print('Hello World!')
